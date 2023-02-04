2a2916a1a3bf9927739b15b4cb27a70c84a320a3
Annie <anniehartmann97@gmail.com>
2023-02-04 11:41:58 +0300

    Upd limit

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,7 @@ const movies = require("../config/movies.json");
 router.get("/movies", async (req, res) => {
   try {
     const page = parseInt(req.query.page) - 1 || 0;
-    const limit = parseInt(req.query.limit) || 5;
+    const limit = parseInt(req.query.limit) || 8;
     const search = req.query.search || "";
     let sort = req.query.sort || "rating";
     let genre = req.query.genre || "All";
